f9d8d0b495c34b4113183b17cf416d677a96d85d
bopzen <boppp@abv.bg>
2025-03-09 23:48:09 +0200

    Finished cv pdf download and fixed minor bugs

--- src/download-pdf.js
+++ src/download-pdf.js
@@ -1,8 +1,28 @@
 function downloadPDF() {
-    const element = document.querySelector('.cv-container');
-    if (element) {
-        html2pdf().from(element).save("my_cv.pdf");
-    } else {
-        console.error("Element with class 'cv-container' not found!");
-    }
-}
+    // Element to render
+    const element = document.getElementById('cv');
+    
+    // Options
+    const options = {
+      margin: [-10, -10, 0, 0],
+      filename: 'cv_boyan_todorov.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 3, useCORS: true },
+      jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' },
+    //    pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
+    };
+    
+    // Generate PDF
+    html2pdf()
+    .from(element)
+    .set(options)
+    .toPdf()
+    .get('pdf')
+    .then(pdf => {
+        const totalPages = pdf.internal.getNumberOfPages();
+        if (totalPages > 2) {
+            pdf.deletePage(totalPages); // Remove last blank page
+        }
+    })
+    .save();
+  }
